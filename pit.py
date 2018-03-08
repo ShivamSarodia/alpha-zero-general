0c7c21076c23011bb63c93b3ccb1d5650cf4ca32
Shivam Sarodia <ssarodia@gmail.com>
2018-03-08 17:36:42 -0500

    update pit

--- pit.py
+++ pit.py
@@ -1,8 +1,7 @@
 import Arena
 from MCTS import MCTS
-from othello.OthelloGame import OthelloGame, display
-from othello.OthelloPlayers import *
-from othello.pytorch.NNet import NNetWrapper as NNet
+from generals.GeneralsGame import GeneralsGame
+from generals.GeneralsNN import NNetWrapper as NNet
 
 import numpy as np
 from utils import *
@@ -12,26 +11,31 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-g = OthelloGame(6)
+class RandomPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        a = np.random.randint(self.game.getActionSize())
+        valids = self.game.getValidMoves(board, 1)
+        while valids[a]!=1:
+            a = np.random.randint(self.game.getActionSize())
+        return a
+
+def display(board):
+    board.show("?")
+
+g = GeneralsGame(5, 5)
 
 # all players
 rp = RandomPlayer(g).play
-gp = GreedyOthelloPlayer(g).play
-hp = HumanOthelloPlayer(g).play
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
+n1.load_checkpoint('./temp/','checkpoint_18.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
-
-
-#n2 = NNet(g)
-#n2.load_checkpoint('/dev/8x50x25/','best.pth.tar')
-#args2 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
-#mcts2 = MCTS(g, n2, args2)
-#n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
 arena = Arena.Arena(n1p, hp, g, display=display)
 print(arena.playGames(2, verbose=True))
